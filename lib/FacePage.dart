8bec141ee394fbc708b2c3fd1a0a830738388de1
Rami Ismael <rami.m.ismael@gmail.com>
2021-03-19 10:53:05 -0500

    Mix the camera tutorial and mlkit lfutter tutorial

--- lib/FacePage.dart
+++ lib/FacePage.dart
@@ -1,13 +1,17 @@
 import 'dart:io';
 
+import 'package:camera/camera.dart';
 import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class FacePage extends StatefulWidget {
-
-
+    final CameraDescription camera;
+  const FacePage({
+  Key key,
+    @required this.camera,
+   }) : super(key: key);
   //StatefulWidget createState() =>ImagesAndFaces(imageFile: _imageFile,faces: _faces,);
 
 
@@ -87,7 +91,10 @@ class FaceCoordinate extends StatelessWidget{
 class _FacePageState extends State<FacePage>{
   File _imageFile;
   List<Face> _faces;
-
+  // Add two variables to the state class to store the CameraController and
+  // the Future.
+  CameraController _controller;
+  Future<void> _initializeControllerFuture;
   // var imageFromCamera = null;
   //to access the camera
   final picker = ImagePicker();
@@ -96,9 +103,32 @@ class _FacePageState extends State<FacePage>{
    * Load image for processing
    * Perform Face Detection
    */
+  @override
+  void initState() {
+    super.initState();
+    // To display the current output from the camera,
+    // create a CameraController.
+    _controller = CameraController(
+      // Get a specific camera from the list of available cameras.
+      widget.camera,
+      // Define the resolution to use.
+      ResolutionPreset.medium,
+    );
+
+    // Next, initialize the controller. This returns a Future.
+    _initializeControllerFuture = _controller.initialize();
+  }
+  @override
+  void dispose() {
+    // Dispose of the controller when the widget is disposed.
+    _controller.dispose();
+    super.dispose();
+  }
   void getImageAndDetectFaces() async {
     //PickedFile imageFromCamera;
     final imageFromCamera = await picker.getImage(source: ImageSource.camera);
+    //final image = await
+    print("imageFromCamera");
     print(imageFromCamera.runtimeType);
     //convert a pckedFile object to File object
     File imageFile = File(imageFromCamera.path);
@@ -123,7 +153,23 @@ class _FacePageState extends State<FacePage>{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return ImageScaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Image Scaffold2.0"),
+      ),
+      body: FutureBuilder<void>(
+      future: _initializeControllerFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          // If the Future is complete, display the preview.
+          return CameraPreview(_controller);
+        } else {
+          // Otherwise, display a loading indicator.
+          return Center(child: CircularProgressIndicator());
+        }
+      }),
+      floatingActionButton: cameraFloatingActionButton(),
+    );
   }
   
 }
--- lib/TakePictureScreen.dart
+++ lib/TakePictureScreen.dart
@@ -79,33 +79,33 @@ class TakePictureScreenState extends State<TakePictureScreen>  {
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.camera_alt_outlined),
         onPressed: () async {
-    // Take the Picture in a try / catch block. If anything goes wrong,
-    // catch the error.
-       try {
+        // Take the Picture in a try / catch block. If anything goes wrong,
+        // catch the error.
+          try {
     // Ensure that the camera is initialized.
-    await _initializeControllerFuture;
+          await _initializeControllerFuture;
 
-    // Attempt to take a picture and get the file `image`
-    // where it was saved.
-    final image = await _controller.takePicture();
+            // Attempt to take a picture and get the file `image`
+            // where it was saved.
+            final image = await _controller.takePicture();
 
-    // If the picture was taken, display it on a new screen.
-    Navigator.push(
-    context,
-    MaterialPageRoute(
-        builder: (context) => DisplayPictureScreen(
-    // Pass the automatically generated path to
-    // the DisplayPictureScreen widget.
-    imagePath: image?.path,
-    ),
-    ),
-    );
-    } catch (e) {
-    // If an error occurs, log the error to the console.
-    print(e);
-    }
-    },
-    ));
+            // If the picture was taken, display it on a new screen.
+            Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => DisplayPictureScreen(
+      // Pass the automatically generated path to
+      // the DisplayPictureScreen widget.
+      imagePath: image?.path,
+              ),
+      ),
+      );
+      } catch (e) {
+      // If an error occurs, log the error to the console.
+      print(e);
+      }
+      },
+      ));
   }
   
 }
